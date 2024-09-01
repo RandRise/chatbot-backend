e9b2547dae36af1e696ff211615bd16fe1d43222
RandRise <randissa7@gmail.com>
2024-09-01 10:06:25 +0300

    Fetch all orders for adminstrator done

--- Controllers/FetchOrders.js
+++ Controllers/FetchOrders.js
@@ -1,4 +1,4 @@
-const { getOrdersByUserId } = require('../Models/FetchOrders');
+const { getOrdersByUserId, getOrders } = require('../Models/FetchOrders');
 const { createResponse } = require('../Utils/responseUtils');
 
 const fetchUserOrders = async (req, res) => {
@@ -16,6 +16,21 @@ const fetchUserOrders = async (req, res) => {
     }
 };
 
+const fetchAllOrders = async (req, res) => {
+
+    try {
+
+        const orders = await getOrders();
+        return res.status(200).json(createResponse(200, 'Orders Fetched', orders));
+
+    } catch (error) {
+
+        console.error('Error fetching user orders:', error);
+        return res.status(500).json(createResponse(500, 'Server Error', null));
+    }
+};
+
 module.exports = {
     fetchUserOrders,
+    fetchAllOrders,
 };
