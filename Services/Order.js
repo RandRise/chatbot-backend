62b5f83aa7b4298078d62d812959e3a89e204f9b
RandRise <randissa7@gmail.com>
2024-09-16 12:57:28 +0300

    Stripe Payment Simulation done

--- Services/Order.js
+++ Services/Order.js
@@ -2,7 +2,7 @@ const { createOrder } = require('../Models/Order');
 const { getPackageById } = require('../Models/Package');
 const { getFreePackage } = require('../Models/Package')
 const stripe = require('stripe')
-('sk_test_51PzPPtJi4fqiBN462OigNpZD6G6xIFC3g9OPfeSMKgvJn3ijIxSoa45mzqVQKOw5xhNdnyH9s66Gmvd1JwRW7zva00pBXC8173')
+    ('sk_test_51PzPPtJi4fqiBN462OigNpZD6G6xIFC3g9OPfeSMKgvJn3ijIxSoa45mzqVQKOw5xhNdnyH9s66Gmvd1JwRW7zva00pBXC8173')
 
 const placeOrder = async (userId, packageId, domain) => {
     try {
@@ -46,7 +46,7 @@ const placeFreeOrder = async (userId, domain) => {
     }
 };
 const stripePayment = async (package) => {
-   
+
     try {
         const priceInCents = Math.round(package.price * 100); // e.g., 19.99 becomes 1999
         const session = await stripe.checkout.sessions.create({
@@ -56,7 +56,7 @@ const stripePayment = async (package) => {
                     currency: 'usd',
                     product_data: {
                         name: package.name,
-                        description: `This Plan Provides${package.msgcount}`,
+                        description: `Unlock the full potential of our AI chatbot with the ${package.name} plan. This plan includes ${package.msgcount} messages, allowing you to interact with your bot seamlessly over a duration of ${package.numofmonths} month(s). Perfect for businesses and individuals looking to optimize their chatbot usage with extended features and support. Enjoy reliable performance, dedicated customer support, and regular updates. Upgrade now to enhance your chatbot experience!`,
                     },
                     unit_amount: priceInCents, // Price in cents
                 },
@@ -64,13 +64,13 @@ const stripePayment = async (package) => {
             }],
             mode: 'payment',
             success_url: 'http://localhost:3001/user/bots',
-            cancel_url: 'http://localhost:3001/cancel',
+            cancel_url: 'http://localhost:3001/user/bots',
         });
         console.log(session.url)
         return (session.url);
-        
-        
-    
+
+
+
     } catch (error) {
         console.error(error);
     }
